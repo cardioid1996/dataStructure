01ccc2cc13b4c2d3760f0d4f073b68352ba93633
cardioid1996 <892144672@qq.com>
2020-03-02 12:51:12 +0800

    改成同步容器，并且用了类condition机制

--- src/com/ds/Concurrent/ProducerConsumerQueue.java
+++ src/com/ds/Concurrent/ProducerConsumerQueue.java
@@ -1,96 +1,66 @@
 package com.ds.Concurrent;
 
 import java.util.LinkedList;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Queue;
 
+public class ProducerConsumerQueue<E> {
 
-public class ProducerConsumerQueue<T> {
+    private Queue<E> queue;
+    private int max = 16;
+    private Object notFull;
+    private Object notEmpty;
 
-    static AtomicInteger workLoad = new AtomicInteger(100);
+    public ProducerConsumerQueue(int size){
+        queue = new LinkedList<>();
+        this.max = size;
+        notFull = new Object();
+        notEmpty = new Object();
+    }
+
+    public synchronized void put(E element){
+        while(queue.size() == max){
+            try {
+                notFull.wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        queue.add(element);
+        notEmpty.notifyAll();
+    }
+
+    public synchronized E take(){
+        while(queue.size()==0){
+            try {
+                notEmpty.wait();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        E element = queue.remove();
+        notFull.notifyAll();
+        return element;
+    }
+
 
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        int capacity = 8;
+        ProducerConsumerQueue<Object> pc = new ProducerConsumerQueue(10);
 
-        ExecutorService service = Executors.newFixedThreadPool(15);
-        for (int i=0; i<5; ++i){
-            service.submit(new Producer(list, capacity));
-        }
-        for(int i=0; i<5; ++i){
-            service.submit(new Consumer(list));
-        }
-        while (true){
-            if (workLoad.get()<=0 && list.isEmpty()){
-                service.shutdownNow();
-                return;
-            }
-        }
-    }
+        final Runnable producer = ()->{
+            while(true)
+                pc.put(new Object());
+        };
 
-
-
-    // 为什么是静态内部类
-    public static class Producer implements Runnable{
-        private final LinkedList<Integer> list;
-        private final int capacity;
-
-        public Producer(LinkedList<Integer> list, int capacity){
-            this.list = list;
-            this.capacity = capacity;
-        }
-        @Override
-        public void run(){
-            while(workLoad.get() > 0){
-                synchronized (list){
-                    while (list.size() == capacity){
-                        try {
-                            list.wait();
-                        }catch (InterruptedException e){
-                            e.printStackTrace();
-                        }
-                    }
-                    if (workLoad.get() > 0){
-                        Integer num = new Random().nextInt();
-                        list.add(num);
-                        workLoad.decrementAndGet();
-                        System.out.println(Thread.currentThread().getName() + " produce " +num + "workload:" + workLoad);
-                        list.notifyAll();
-                    }
-                }
-            }
-        }
-    }
-
-    public static class Consumer implements Runnable{
-        private final LinkedList<Integer> list;
-
-        public Consumer(LinkedList<Integer> list){
-            this.list = list;
-        }
-
-        @Override
-        public void run(){
+        final Runnable consumer = ()->{
             while(true){
-                synchronized (list){
-                    while (list.isEmpty()){
-                        if (workLoad.get()<=0)
-                            return;
-                        try {
-                            list.wait();
-                        }catch (InterruptedException e){
-                            e.printStackTrace();
-                        }
-                    }
-                    int num = list.removeFirst();
-                    System.out.println(Thread.currentThread().getName() + " consume " + num);
-                    list.notifyAll();
-                    if (workLoad.get()<=0 && list.isEmpty())
-                        return;
-                }
+                Object obj = pc.take();
+                // process(obj)
             }
-        }
+        };
+
+        new Thread(producer).start();
+        new Thread(consumer).start();
     }
+
+
 }
